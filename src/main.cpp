673d851698197af81f9eefab2e6ad2c091bc54c8
luoxiaohei31 <847289603@qq.com>
2024-03-04 13:50:59 +0800

    第一个三角形

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <clocale>
 #endif
 
+#define ON
+#ifndef ON
+
 int main() {
 #ifdef _WIN32
     try {
@@ -151,3 +154,98 @@ int main() {
     glfwTerminate();
     return 0;
 }
+#endif  // ON
+
+#ifndef OFF
+
+static void render() {
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 0.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.5f, -0.5f, 0.0f);
+    CHECK_GL(glEnd());
+
+}
+
+int main() {
+    if (!glfwInit()) {
+        const char* errmsg;
+        glfwGetError(&errmsg);
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "failed to initialize GLFW: " << errmsg << '\n';
+        return -1;
+    }
+
+    // hint the version required: OpenGL 2.0
+    constexpr int version = 20;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+    glfwWindowHint(GLFW_VERSION_MAJOR, version / 10);
+    glfwWindowHint(GLFW_VERSION_MINOR, version % 10);
+    if (version >= 33)
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window
+    GLFWwindow* window = glfwCreateWindow(640, 640, "主窗口", nullptr, nullptr);
+    if (!window) {
+        const char* errmsg;
+        glfwGetError(&errmsg);
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "GLFW failed to create window" << errmsg << '\n';
+        std::cerr << "============================================\n";
+        if (!strcmp(errmsg, "X11: The DISPLAY environment variable is missing")) {
+            std::cerr << "You seems not running with graphic display\n";
+        }
+        else if (!strcmp(errmsg, "WGL: The driver does not appear to support OpenGL")) {
+            std::cerr << "Please consider install an OpenGL driver,or use mesa driver\n";
+        }
+        else if (!strcmp(errmsg, "WGL: Failed to create OpenGL context")) {
+            std::cerr << "Your driver seems not supporting the required OpenGL version\n";
+        }
+        std::cerr << "- If you have a physical graphic card (e.g. NVIDIA), install it from your graphic card vendor official website: http://www.nvidia.com/Download/index.aspx\n";
+        std::cerr << "- If you are using Windows, download opengl32.dll from https://pan.baidu.com/s/1TZ6nVJC7DZIuUarZrGJYow?pwd=opgl and place it into the same directory as this executable file (alternatively you may download opengl32sw.dll from Internet and rename it to opengl32.dll to place into the same directory as this executable file)\n";
+        std::cerr << "- If you are using Linux or WSL1, install the mesa driver: https://ubuntuhandbook.org/index.php/2021/07/install-latest-mesa-ubuntu-20-04-21-04/";
+        std::cerr << "- If you use WSL2, install WSLg: https://learn.microsoft.com/zh-cn/windows/wsl/tutorials/gui-apps\n";
+        std::cerr << "- If you are using SSH remote server, try connect it using ssh -X <ip address>\n";
+        std::cerr << "- If you are using MacOS, you probably want to use Windows or Linux instead for better OpenGL support\n";
+        std::cerr << "- If you are using a Laptop with dual-cards, make sure you have switch to dedicated card (NVIDIA) instead of the integrated card (Intel)\n";
+        std::cerr << "==============================================\n";
+        system("pause");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Load glXXX function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwTerminate();
+        std::cerr << "GLAD failed to load GL function\n";
+        return -1;
+    }
+    std::cerr << "OpenGL Version: " << glGetString(GL_VERSION) << '\n';
+
+    CHECK_GL(glEnable(GL_POINT_SMOOTH));
+    CHECK_GL(glEnable(GL_BLEND));
+    CHECK_GL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    CHECK_GL(glPointSize(64.0f));
+
+    // start main game loop
+    while (!glfwWindowShouldClose(window)) {
+        // render graphics
+        CHECK_GL(glClear(GL_COLOR_BUFFER_BIT));
+        render();
+
+        // refresh screen
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
+#endif // !OFF
+
